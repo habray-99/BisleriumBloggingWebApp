fa5deb017511af6b47ed4cf4efd285eb6f99306b
Zankoden <zankodits@gmail.com>
2024-04-27 15:06:55 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BisleriumBloggingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
