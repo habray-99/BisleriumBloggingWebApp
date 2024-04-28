ecc37d1e1feb9981cac65761e1c43028f8150fe5
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-28 19:56:46 +0545

    Add Role and Address properties to CustomUser class in CustomUser.cs

--- Areas/Identity/Data/CustomUser.cs
+++ Areas/Identity/Data/CustomUser.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using WebApplication6.Models;
 
 namespace BisleriumBloggingWebApp.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the CustomUser class
 public class CustomUser : IdentityUser
 {
+    [Required]
+    [MaxLength(10)]
+    public string? Role { get; set; }
+    [ProtectedPersonalData]
+    [MaxLength(30)]
+    public string? Address { get; set; }
+    public string? ProfilePicture { get; set; }
+    public ICollection<Blog>? Blogs { get; set; }
+    public ICollection<Comment>? Comments { get; set; }
+    public ICollection<Reaction>? Reactions { get; set; }
+    public ICollection<Notification>? Notifications { get; set; }
+    public ICollection<CommentReaction>? CommentReactions { get; set; }
 }
 
