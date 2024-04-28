bd0c1d80af884048b23808909a37bb7697360669
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-28 19:57:05 +0545

    Add necessary using statements for Microsoft.AspNetCore.Mvc.TagHelpers in Identity pages

--- Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -2,16 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using BisleriumBloggingWebApp.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
 
 namespace BisleriumBloggingWebApp.Areas.Identity.Pages.Account
 {
